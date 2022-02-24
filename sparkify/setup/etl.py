ccf54299f3ba3549101fefd261aa14e38adc02af
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-23 20:36:20 -0600

    Add empty strings to etl.py's functions where input is expected in exercise for now

--- sparkify/setup/etl.py
+++ sparkify/setup/etl.py
@@ -12,37 +12,37 @@ from sql_queries import *
 ############------------ FUNCTION(S) ------------############
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = ''
 
     # insert song record
-    song_data = 
+    song_data = ''
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = ''
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = ''
 
     # filter by NextSong action
-    df = 
+    df = ''
 
     # convert timestamp column to datetime
-    t = 
+    t = ''
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = ''
+    column_labels = ''
+    time_df = ''
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = ''
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -61,7 +61,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = ''
         cur.execute(songplay_table_insert, songplay_data)
 
 
